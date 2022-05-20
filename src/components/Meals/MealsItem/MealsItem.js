66170db3bdedd0d9a02dfad2a97dbf9dda3023a6
AhmetNishefci <metti.ni@hotmail.com>
2022-05-20 14:02:25 +0200

    Adding items to cart

--- src/components/Meals/MealsItem/MealsItem.js
+++ src/components/Meals/MealsItem/MealsItem.js
@@ -1,7 +1,19 @@
+import React, { useContext } from "react";
+import CartContext from "../../../store/cart-context";
 import classes from "./MealsItem.module.css";
 import MealsItemForm from "./MealItemForm";
 const MealsItem = (props) => {
+  const CartCtx = useContext(CartContext);
   const price = `$${props.price}`;
+
+  const addToCartHandler = (amount) => {
+    CartCtx.addItem({
+      id: props.id,
+      name: props.name,
+      amount: amount,
+      price: props.id,
+    });
+  };
 
   return (
     <li className={classes.meal}>
@@ -11,7 +23,7 @@ const MealsItem = (props) => {
         <div className={classes.price}>{price}</div>
       </div>
       <div>
-        <MealsItemForm id={props.id} />
+        <MealsItemForm id={props.id} onAddToCart={addToCartHandler} />
       </div>
     </li>
   );
